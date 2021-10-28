1ee6608389f6ae59e74d31949b9965b31a1ba082
YBQ4027 <75928118+YBQ4027@users.noreply.github.com>
2021-10-28 22:40:08 +0800

    Create sensorfusion_mahony.h

---
+++ FLIGHT/inc/sensorfusion_mahony.h
@@ -0,0 +1,8 @@
+#ifndef _SENSORFUSION_MAHONY_H_
+#define _SENSORFUSION_MAHONY_H_
+#include "config_parameter.h"
+
+ void Update_AHRS_mahony(sensor_data* sensordata);
+ void Update_IMU_mahony(sensor_data* sensordata);
+
+#endif	/*  _SENSORFUSION_MAHONY_H_ */
